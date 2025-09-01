f801ae902065c780297f2eefc20283aca250ea7a
papchenko <nikopapchenko@gmail.com>
2025-09-01 09:50:41 +0300

    Updates

--- service-worker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener("push", (event) => {
-  const data = event.data ? event.data.json() : {};
-  event.waitUntil(
-    self.registration.showNotification(data.title || "Secret place!", {
-      body: data.body || "You have entered a secret area!",
-      icon: "/icon.png",
-    })
-  );
-});
